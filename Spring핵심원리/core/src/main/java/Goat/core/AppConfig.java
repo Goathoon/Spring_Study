a3b2d1db12a61ccbcebfd67d08fcde20b1a7fede
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-10 12:20:12 +0900

    스프링스럽게 만들기 맛만봄 section3

--- Spring핵심원리/core/src/main/java/Goat/core/AppConfig.java
+++ Spring핵심원리/core/src/main/java/Goat/core/AppConfig.java
@@ -1,18 +1,37 @@
 package Goat.core;
 
-import Goat.core.discount.FixDiscountPolicy;
+import Goat.core.discount.DiscountPolicy;
+import Goat.core.discount.RateDiscountPolicy;
+import Goat.core.member.MemberRepository;
 import Goat.core.member.MemberService;
 import Goat.core.member.MemberServiceImpl;
 import Goat.core.member.MemoryMemberRepository;
 import Goat.core.order.OrderService;
 import Goat.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    private static MemberRepository memberRepository() { //굳이 public으로 ?
+        return new MemoryMemberRepository();
     }
+
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    private static DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
 }
--- Spring핵심원리/core/src/main/java/Goat/core/MemberApp.java
+++ Spring핵심원리/core/src/main/java/Goat/core/MemberApp.java
@@ -5,11 +5,16 @@ import Goat.core.member.Member;
 import Goat.core.member.MemberService;
 import Goat.core.member.MemberServiceImpl;
 import Goat.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); //스프링 컨테이너 ApplicationContext
+        MemberService memberService = applicationContext.getBean("m emberService", MemberService.class);//메소드 이름을 이름 등록(첫번째 인자) 두번째인자는 반환 타입
 
         Member member = new Member(1L,"GOAT", Grade.VIP);
         memberService.join(member);
