9e766320fdccc94699b0da6601b54fe4ee0516ee
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-03-04 22:14:23 +0900

    생성자 옵션 처리

---
+++ Spring_haeksimwonri/core/src/test/java/Goat/core/autowired/AutowiredTest.java
@@ -0,0 +1,42 @@
+package Goat.core.autowired;
+
+import Goat.core.member.Member;
+import Goat.core.member.MemberService;
+import Goat.core.member.MemberServiceImpl;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Optional;
+
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean{
+        @Autowired(required=false)
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3){
+            System.out.println("noBean2 = " + noBean3);
+        }
+
+        @Autowired(required=false)
+        public void setYesBean(MemberServiceImpl memberServiceImpl){
+            System.out.println("memberService = " + memberServiceImpl);
+        }
+    }
+}
