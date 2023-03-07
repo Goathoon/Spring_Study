f4704199d0b0b0e1f03aa30cd1be46e08b314142
Goathoon <ownest2@gmail.com>
2023-03-07 22:08:13 +0900

    스프링 빈 스코프 공부 완료

--- Spring_haeksimwonri/core/src/test/java/Goat/core/lifecycle/NetworkClient.java
+++ Spring_haeksimwonri/core/src/test/java/Goat/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package Goat.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url; //접속하려고 하는 url
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " +url);
-        connect();
-        call("초기화 연결 메세지");
     }
     public void setUrl(String url){
         this.url = url;
@@ -25,5 +26,17 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: " +url);
+    }
+
+    @PostConstruct
+    public void happy(){
+        System.out.println("NetworkClient.happy");
+        connect();
+        call("초기화 연결 메세지");
+    }
+    @PreDestroy
+    public void terrify() {
+        System.out.println("NetworkClient.terrify");
+        disconnect();
     }
 }
